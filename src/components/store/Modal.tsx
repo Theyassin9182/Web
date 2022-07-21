b03f26ff44cd671d89a42a0194cc3c4e9e09764f
InBlue <InBlueOfficial@Outlook.com>
2022-07-21 22:04:23 +1000

    Close store modal when clicking add to cart

--- src/components/store/Modal.tsx
+++ src/components/store/Modal.tsx
@@ -109,6 +109,7 @@ export default function Modal({ productId, add }: Props) {
 								onClick={(e) => {
 									e.preventDefault();
 									add();
+									details.current?.parentElement?.closest("dialog")?.close();
 								}}
 							>
 								Add to cart
