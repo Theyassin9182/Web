0a53754ed2b4a7c55b0021968ee4ec5385f5f9ed
InBlue <InBlueOfficial@Outlook.com>
2022-07-23 18:59:52 +1000

    Fix hydration error

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,8 +1,12 @@
 import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
 import Link from "./ui/Link";
 
 export default function Footer() {
+	const [mounted, setMounted] = useState(false);
 	const { theme, setTheme } = useTheme();
+
+	useEffect(() => setMounted(true), []);
 
 	return (
 		<footer>
@@ -11,39 +15,27 @@ export default function Footer() {
 					<div className="flex flex-col items-center space-x-0 space-y-4 p-14 lg:flex-row lg:space-x-72 lg:space-y-0">
 						<div className="flex items-center space-x-4">
 							<Link href="/">
-								<img
-									src={"/img/memer.png"}
-									alt="Logo"
-									width="100"
-									height="100"
-								/>
+								<img src={"/img/memer.png"} alt="Logo" width="100" height="100" />
 							</Link>
 							<div className="flex flex-col -space-y-1">
 								<h2 className="font-montserrat text-2xl font-bold text-dank-300 dark:text-light-200">
 									DANK MEMER
 								</h2>
 								<span className="text-md text-gray-400">
-									Copyright © {new Date().getFullYear()} Dank
-									Memer
+									Copyright © {new Date().getFullYear()} Dank Memer
 								</span>
 								<span data-ccpa-link="1"></span>
 							</div>
 						</div>
 						<div className="flex space-x-6 lg:space-x-12">
 							<div className="flex flex-col space-y-0">
-								<Link
-									href="https://www.patreon.com/join/dankmemerbot"
-									variant="secondary"
-								>
+								<Link href="https://www.patreon.com/join/dankmemerbot" variant="secondary">
 									Premium
 								</Link>
 								<Link href="/commands" variant="secondary">
 									Commands
 								</Link>
-								<Link
-									href="/community/blog"
-									variant="secondary"
-								>
+								<Link href="/community/blog" variant="secondary">
 									Our blog
 								</Link>
 							</div>
@@ -77,19 +69,13 @@ export default function Footer() {
 									We're Hiring!
 								</Link>
 								<Link href="#" variant="secondary">
-									<span
-										onClick={() =>
-											setTheme(
-												theme === "dark"
-													? "light"
-													: "dark"
-											)
-										}
-									>
-										{theme === "dark"
-											? "Light Mode"
-											: "Dark Mode"}
-									</span>
+									{mounted ? (
+										<span onClick={() => setTheme(theme === "dark" ? "light" : "dark")}>
+											{theme === "dark" ? "Light Mode" : "Dark Mode"}
+										</span>
+									) : (
+										<span>Loading...</span>
+									)}
 								</Link>
 							</div>
 						</div>
